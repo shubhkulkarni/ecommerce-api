38fafcde94cda2c68e3078ab0171149f7f0ea723
SHUBHAM <shubhamkulkarni.sh@gmail.com>
2021-02-09 11:14:16 +0530

    errorhandle#1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const productRouter = require("./src/routes/productRoutes");
+const AppError = require("./src/utils/Error");
+const { errMiddleware } = require("./src/middlewares/error");
 const app = express();
 dotenv.config({ path: "./config.env" });
 app.use(express.json());
@@ -8,11 +10,12 @@ require("./database");
 
 app.use("/api/products", productRouter);
 
-app.all("*", (req, res) => {
-  res
-    .status(404)
-    .json({ status: 404, message: "invalid url : " + req.originalUrl });
+app.all("*", (req, res, next) => {
+  next(new AppError("invalid url : " + req.originalUrl, 404));
 });
+
+app.use(errMiddleware);
+
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on ${process.env.PORT}`)
 );
