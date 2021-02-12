a2813464b7c3f29971971825613bf7f525b1f933
SHUBHAM <shubhamkulkarni.sh@gmail.com>
2021-02-12 21:12:36 +0530

    indexing

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -40,7 +40,20 @@ const productSchema = new mongoose.Schema(
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+//indexes are very important to improve read performance
+
+productSchema.index({ price: 1, stock: -1 }); //1 for ascending order   -1 for descending order
+
+// productSchema.index({...},{unique:true})   //this allows to set the combination should be unique
+
+// index reduces no. of documents to be examined, so performance increases
+//indexes should be set to fields which are queried/accessed the most
+
+//static methods always call on the Model
+//query.explain() to get query performance statistics
+
 productSchema.virtual("discount").get(function () {
+  //discount will only appear in getresults, and not in Database
   return this.price * 0.1;
 });
 const Product = mongoose.model("Product", productSchema);
